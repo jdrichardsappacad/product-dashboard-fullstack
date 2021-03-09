c6253e1f2ffa5587522181048b5ebdd47b6fd0dd
jdrichardstech <jdrichardstech@gmail.com>
2021-03-09 16:06:11 -0500

    remove inline css and update css classname

--- frontend/src/components/CreateProduct/index.js
+++ frontend/src/components/CreateProduct/index.js
@@ -22,7 +22,7 @@ const CreateProduct = () => {
   };
 
   return (
-    <div className='add'>
+    <div className='add-product'>
       <h3>Add A Product</h3>
       <form onSubmit={handleSubmit} className='add-product'>
         <input
