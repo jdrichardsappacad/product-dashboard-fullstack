18387d1c12560659ac160b04010ff8dd43500f79
jdrichardstech <jdrichardstech@gmail.com>
2021-03-09 18:38:52 -0500

    fix navigation component

--- frontend/src/Navigation/index.js
+++ frontend/src/Navigation/index.js
@@ -4,6 +4,8 @@ const Navigation = () => {
     <ul>
       <li>
         <NavLink to='/'>Products</NavLink> |
+      </li>
+      <li>
         <NavLink to='/create'>Create Product</NavLink>
       </li>
     </ul>
