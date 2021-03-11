29b3700c929263485e4c8fed1af1baa81d286009
jdrichardstech <jdrichardstech@gmail.com>
2021-03-11 10:08:27 -0500

    removed directories

--- solution/backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: 'Test index router' });
-});
-
-module.exports = router;
